4675959e37390f438e631013c43d2fa2f427fb42
EduardoFerreira22 <eduardoferreira_of@outlook.com>
2025-02-08 18:05:54 -0300

    Versionando api's e organizando urls em seus respectivos models

--- app/urls.py
+++ app/urls.py
@@ -1,40 +1,12 @@
-"""
-URL configuration for app project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
-from actors.views import ActorCreateListView, ActorRetrieverUpdateDestroyView
-from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
-from reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestroyView
-
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
-    path("genres/<int:pk>", GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
-    
-    path('actors/', ActorCreateListView.as_view(), name='actors-create-list'),
-    path('actors/<int:pk>/', ActorRetrieverUpdateDestroyView.as_view(), name='actors-detail-view'),
-
-    path('movies', MovieCreateListView.as_view(), name='movies-crete-list'),
-    path('movies/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view'),
-
-    path('reviews/', ReviewCreateListView.as_view(), name='review-create-list'),
-    path('reviews/<int:pk>', ReviewRetrieveUpdateDestroyView.as_view(), name='review-detail-view'),
+    path('api/v1/', include('genres.urls')), #Inclui todas as urls do meu arquivo genres o django considera todas as urls do arquivo
+    path('api/v1/',include('actors.urls')),
+    path('api/v1/', include('movies.urls')),
+    path('api/v1/', include('reviews.urls')),
 
 ]
